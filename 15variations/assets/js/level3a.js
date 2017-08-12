d47b206640fac9f633f412b17d59ea268df023c9
custardsieve <info@davidnewman.com.au>
2017-08-12 18:45:51 +1000

    new order

---
+++ 15variations/assets/js/level3a.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+  $('.image-container').click(function() {
+
+    $('.image2').toggleClass('translate');
+    $('.image1').toggleClass('translate');
+    $('span').toggleClass('two');
+//    $('span').replaceWith('<span>1</span>');
+
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });

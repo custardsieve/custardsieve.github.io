10f7132567701f8ed2cd4970665143866d5e6b74
custardsieve <info@davidnewman.com.au>
2017-08-13 16:35:13 +1000

    level4a

---
+++ 15variations/assets/js/level4a.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+  
+  
+    setTimeout(function () {
+      $('div.transform').css("transform","translateY(2%)");
+    }, 1000);
+
+    setTimeout(function () {
+      $('div.base-container').addClass('horizontal');
+    }, 3000);
+
+
+
+
+
+  });

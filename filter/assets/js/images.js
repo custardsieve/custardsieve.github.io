39fe7f93d83f13a30c1f5c9ceb0fd5f86294a2ca
custardsieve <info@davidnewman.com.au>
2017-09-22 15:24:41 +1000

    filter upload

---
+++ filter/assets/js/images.js
@@ -0,0 +1,553 @@
+var npData = [
+{
+  "src": "assets/images/file01.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file02.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file03.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file04.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file05.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file06.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file07.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file08.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file09.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file10.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file11.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file12.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file13.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file14.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file15.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file16.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file17.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file18.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file19.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file20.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file21.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file22.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file23.jpg"
+  , "class": "item gary-w animals"
+}
+, {
+  "src": "assets/images/file24.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file25.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file26.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file27.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file28.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file29.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file30.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file31.jpg"
+  , "class": "item gary-w airports"
+}
+, {
+  "src": "assets/images/file32.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file33.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file34.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file35.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file36.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file37.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file38.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file39.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file40.jpg"
+  , "class": "item lee-f monuments"
+}
+, {
+  "src": "assets/images/file41.jpg"
+  , "class": "item lee-f newyork"
+}
+, {
+  "src": "assets/images/file42.jpg"
+  , "class": "item lee-f newyork"
+}
+, {
+  "src": "assets/images/file43.jpg"
+  , "class": "item lee-f newyork"
+}
+, {
+  "src": "assets/images/file44.jpg"
+  , "class": "item lee-f newyork"
+}
+, {
+  "src": "assets/images/file45.jpg"
+  , "class": "item lee-f newyork"
+}
+, {
+  "src": "assets/images/file46.jpg"
+  , "class": "item lee-f newyork"
+}
+, {
+  "src": "assets/images/file47.jpg"
+  , "class": "item lee-f newyork"
+}
+, {
+  "src": "assets/images/file48.jpg"
+  , "class": "item lee-f self-portraits"
+}
+, {
+  "src": "assets/images/file49.jpg"
+  , "class": "item lee-f self-portraits"
+}
+, {
+  "src": "assets/images/file50.jpg"
+  , "class": "item lee-f self-portraits"
+}
+, {
+  "src": "assets/images/file51.jpg"
+  , "class": "item lee-f self-portraits"
+}
+, {
+  "src": "assets/images/file52.jpg"
+  , "class": "item lee-f self-portraits"
+}
+, {
+  "src": "assets/images/file53.jpg"
+  , "class": "item lee-f self-portraits"
+}
+, {
+  "src": "assets/images/file54.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file55.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file56.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file57.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file58.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file59.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file60.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file61.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file62.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file63.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file64.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file65.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file66.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file67.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file68.jpg"
+  , "class": "item elliot-e"
+}
+, {
+  "src": "assets/images/file69.jpg"
+  , "class": "item mark-c"
+}
+, {
+  "src": "assets/images/file70.jpg"
+  , "class": "item mark-c"
+}
+, {
+  "src": "assets/images/file71.jpg"
+  , "class": "item mark-c"
+}
+, {
+  "src": "assets/images/file72.jpg"
+  , "class": "item mark-c"
+}
+, {
+  "src": "assets/images/file73.jpg"
+  , "class": "item mark-c"
+}
+, {
+  "src": "assets/images/file74.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file75.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file76.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file77.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file78.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file79.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file80.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file81.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file82.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file83.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file84.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file85.jpg"
+  , "class": "item stephen-s"
+}
+, {
+  "src": "assets/images/file86.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file87.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file88.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file89.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file90.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file91.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file92.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file93.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file94.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file95.jpg"
+  , "class": "item joel-m"
+}
+, {
+  "src": "assets/images/file96.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file97.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file98.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file99.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file100.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file101.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file102.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file103.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file104.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file105.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file106.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file107.jpg"
+  , "class": "item ikko-n"
+}
+, {
+  "src": "assets/images/file108.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file109.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file110.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file111.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file112.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file113.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file114.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file115.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file116.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file117.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file118.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file119.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file120.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file121.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file122.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file123.jpg"
+  , "class": "item shomei-t"
+}
+, {
+  "src": "assets/images/file124.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file125.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file126.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file127.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file128.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file129.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file130.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file131.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file132.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file133.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file134.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file135.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file136.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file137.jpg"
+  , "class": "item gary-w women"
+}
+, {
+  "src": "assets/images/file138.jpg"
+  , "class": "item gary-w women"
+}];

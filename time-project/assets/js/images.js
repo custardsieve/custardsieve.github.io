417ec3bea9ae8fcb87e7bbe46aecb8f658a50533
custardsieve <info@davidnewman.com.au>
2017-10-24 15:34:23 +1100

    time-project

---
+++ time-project/assets/js/images.js
@@ -0,0 +1,553 @@
+var dnData = [
+{
+  "src": "assets/images/davidnewman01.jpg"
+  , "class": "item publications thumb typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman02.jpg"
+  , "class": "item publications typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman03.jpg"
+  , "class": "item publications typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman04.jpg"
+  , "class": "item publications typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman05.jpg"
+  , "class": "item publications typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman06.jpg"
+  , "class": "item publications typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman07.jpg"
+  , "class": "item publications typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman08.jpg"
+  , "class": "item publications typographic-reader"
+}
+, {
+  "src": "assets/images/davidnewman09.jpg"
+  , "class": "item publications thumb high-low"
+}
+, {
+  "src": "assets/images/davidnewman10.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman11.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman12.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman13.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman14.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman15.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman16.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman17.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman18.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman19.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman20.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman21.jpg"
+  , "class": "item publications high-low"
+}
+, {
+  "src": "assets/images/davidnewman22.jpg"
+  , "class": "item publications pre-cursor"
+}
+, {
+  "src": "assets/images/davidnewman23.jpg"
+  , "class": "item publications pre-cursor"
+}
+, {
+  "src": "assets/images/davidnewman24.jpg"
+  , "class": "item publications pre-cursor"
+}
+, {
+  "src": "assets/images/davidnewman25.jpg"
+  , "class": "item publications pre-cursor"
+}
+, {
+  "src": "assets/images/davidnewman26.jpg"
+  , "class": "item publications pre-cursor"
+}
+, {
+  "src": "assets/images/davidnewman27.jpg"
+  , "class": "item publications pre-cursor"
+}
+, {
+  "src": "assets/images/davidnewman28.jpg"
+  , "class": "item publications thumb pre-cursor"
+}
+, {
+  "src": "assets/images/davidnewman29.jpg"
+  , "class": "item publications thumb cutting-edge"
+}
+, {
+  "src": "assets/images/davidnewman30.jpg"
+  , "class": "item publications cutting-edge"
+}
+, {
+  "src": "assets/images/davidnewman31.jpg"
+  , "class": "item publications cutting-edge"
+}
+, {
+  "src": "assets/images/davidnewman32.jpg"
+  , "class": "item publications cutting-edge"
+}
+, {
+  "src": "assets/images/davidnewman33.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman34.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman35.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman36.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman37.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman38.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman39.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman40.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman41.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman42.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman43.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman44.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman45.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman46.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman47.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman48.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman49.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman50.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman51.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman52.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman53.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman54.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman55.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman56.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman57.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman58.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman59.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman60.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman61.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman62.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman63.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman64.jpg"
+  , "class": "item visual-identities"
+}
+, {
+  "src": "assets/images/davidnewman65.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman66.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman67.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman68.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman69.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman70.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman71.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman72.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman73.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman74.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman75.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman76.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman77.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman78.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman79.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman80.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman81.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman82.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman83.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman84.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman85.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman86.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman87.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman88.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman89.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman90.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman91.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman92.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman93.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman94.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman95.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman96.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman97.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman98.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman99.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman100.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman101.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman102.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman103.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman104.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman105.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman106.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman107.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman108.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman109.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman110.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman111.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman112.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman113.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman114.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman115.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman116.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman117.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman118.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman119.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman120.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman121.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman122.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman123.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman124.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman125.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman126.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman127.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman128.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman129.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman130.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman131.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman132.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman133.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman134.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman135.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman136.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman137.jpg"
+  , "class": "item"
+}
+, {
+  "src": "assets/images/davidnewman138.jpg"
+  , "class": "item"
+}];
